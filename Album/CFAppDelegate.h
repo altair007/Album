28bab0ba3ad6bb4b9cbf098911dc28a7f1d19927
颜风 <1178328901@qq.com>
2014-05-30 19:50:15 +0800

    Initial Commit

---
+++ Album/CFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CFAppDelegate.h
+//  Album
+//
+//  Created by   颜风 on 14-5-30.
+//  Copyright (c) 2014年 Shadow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
